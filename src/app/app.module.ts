5a87b72f2123e631f37796a9e6bd9a582528160a
wissemgrari <wissemgrari@outlook.com>
2024-03-02 20:32:18 +0100

    Build the core module

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,15 +2,12 @@ import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 
 import { AppComponent } from './app.component';
+import { CoreModule } from './core/core.module';
 
 @NgModule({
-  declarations: [
-    AppComponent
-  ],
-  imports: [
-    BrowserModule
-  ],
+  declarations: [AppComponent],
+  imports: [BrowserModule, CoreModule],
   providers: [],
-  bootstrap: [AppComponent]
+  bootstrap: [AppComponent],
 })
-export class AppModule { }
+export class AppModule {}
---
+++ src/app/core/core.module.ts
@@ -0,0 +1,12 @@
+// Core Module only dealing with services and other core functionality of the application.
+
+import { NgModule } from "@angular/core";
+
+@NgModule({
+    providers: [
+        // Add services here
+        // eg: UserService
+    ]
+})
+
+export class CoreModule {}
