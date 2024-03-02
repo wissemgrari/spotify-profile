87c4d9bf7f26711bd90c71b2e169b3dc931eb27d
wissemgrari <wissemgrari@outlook.com>
2024-03-02 21:46:34 +0100

    Add navbar component and router outlet to app component

--- src/app/components/navigation/navigation.module.ts
+++ src/app/components/navigation/navigation.module.ts
@@ -5,5 +5,6 @@ import { NavbarComponent } from './navbar/navbar.component';
 @NgModule({
   declarations: [NavbarComponent],
   imports: [SharedModule],
+  exports: [NavbarComponent], // export NavbarComponent to use it in other modules
 })
 export class NavigationModule {}
