5a73c21bfec697befffd6cbe1de7d131092b7ebf
wissemgrari <wissemgrari@outlook.com>
2024-03-02 21:10:27 +0100

    Build the navigation module and add the navbar component

--- src/app/components/navigation/navigation.module.ts
+++ src/app/components/navigation/navigation.module.ts
@@ -1,6 +1,6 @@
 import { NgModule } from '@angular/core';
 import { SharedModule } from 'src/app/shared/shared.module';
-import { NavbarComponent } from '../navbar/navbar.component';
+import { NavbarComponent } from './navbar/navbar.component';
 
 @NgModule({
   declarations: [
