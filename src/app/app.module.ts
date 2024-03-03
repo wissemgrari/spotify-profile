552f332732e2f455cca0920084fabd4b3168c7d7
wissemgrari <wissemgrari@outlook.com>
2024-03-03 15:10:56 +0100

    Build profile module

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -5,10 +5,17 @@ import { AppComponent } from './app.component';
 import { CoreModule } from './core/core.module';
 import { NavigationModule } from './components/navigation/navigation.module';
 import { AppRoutingModule } from './app-routing.module';
+import { ProfileModule } from './components/profile/profile.module';
 
 @NgModule({
   declarations: [AppComponent],
-  imports: [BrowserModule, AppRoutingModule, CoreModule, NavigationModule],
+  imports: [
+    BrowserModule,
+    AppRoutingModule,
+    CoreModule,
+    NavigationModule,
+    ProfileModule,
+  ],
   bootstrap: [AppComponent],
 })
 export class AppModule {}
---
+++ src/app/components/profile/profile.module.ts
@@ -0,0 +1,10 @@
+import { NgModule } from '@angular/core';
+import { ProfileComponent } from './profile.component';
+import { SharedModule } from 'src/app/shared/shared.module';
+
+@NgModule({
+  declarations: [ProfileComponent],
+  imports: [SharedModule],
+  exports: [ProfileComponent]
+})
+export class ProfileModule {}
