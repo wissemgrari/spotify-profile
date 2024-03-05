2f4e0736107f415b8d27201e3bd76444a6aa05e2
wissemgrari <wissemgrari@outlook.com>
2024-03-05 20:28:29 +0100

    build the tracks UI

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,12 +1,13 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
-import { ProfileComponent } from './components/profile/profile/profile.component';
+import { ProfileComponent } from './components/profile/profile.component';
 import { ArtistsComponent } from './components/artists/artists.component';
+import { TracksComponent } from './components/tracks/tracks.component';
 
 const routes: Routes = [
   { path: '', component: ProfileComponent },
   { path: 'artists', component: ArtistsComponent },
-  // { path: 'tracks', component: TracksComponent },
+  { path: 'tracks', component: TracksComponent },
   // { path: 'recent', component: RecentComponent },
   // { path: 'playlists', component: PlaylistsComponent },
 ];
