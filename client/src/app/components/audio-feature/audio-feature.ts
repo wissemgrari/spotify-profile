7bb50ff9a1b12488ef6d9916045d572136b21c2a
wissemgrari <wissemgrari@outlook.com>
2024-04-12 21:35:31 +0100

    initialiaze audio-feature chart component

---
+++ client/src/app/components/audio-feature/audio-feature.ts
@@ -0,0 +1,10 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'audio-feature',
+  templateUrl: './audio-feature.html',
+  styleUrls: ['./audio-feature.component.css']
+})
+export class AudioFeature {
+
+}
